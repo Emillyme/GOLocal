b3839d55505c631c473d92398196f80922e8f910
Emilly Mello <emill.ymello212@gmail.com>
2023-10-14 00:52:09 -0300

    Navbar detalhes mobile

--- frontweb/src/Components/Navbar/Navbar.jsx
+++ frontweb/src/Components/Navbar/Navbar.jsx
@@ -16,10 +16,21 @@ const Navbar = () => {
         setActive('navBar')
     }
 
+// código para navbar ficar com efeito quando descer
+    const [transparent, setTransparent] = useState('header')
+    const addBg = () => {
+        if(window.scrollY >= 10){
+            setTransparent('header activeHeader')
+        }
+        else{
+            setTransparent('header')
+        }
+    }
+    window.addEventListener('scroll', addBg);
+
     return(
-        <>
         <section className="navBarSection">
-            <div className="header">
+            <div className={transparent}>
                 <div className="logoDiv">
                    <div a href="#" className="logo">
                     <h1 className="flex">
@@ -71,7 +82,6 @@ const Navbar = () => {
                 </div>
             </div>
         </section>
-        </>
     )
 }
 
